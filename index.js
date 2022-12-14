d5e4c246fe0b5f0d1ce0c8404d9c9a9c5b20b7e4
Umarbek Rakhmatov <umarbek1306@gmail.com>
2022-12-13 20:54:32 -0500

    commit tree 3134607974a8be8c1d259c28d7d94a055b5b71b1

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const cardsFor = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cardsFor, event) {
+    let finishedCards = [];
+    for (let i = 0; i < cardsFor.length; i++) {
+        finishedCards.push(`Thank you, ${cardsFor[i]}, for the wonderful ${event} gift!`);
+        console.log(finishedCards);
+    }
+    return finishedCards;
+}
+
+writeCards(cardsFor, "surprise");
+
+function countDown(x) {
+    while (x >= 0) {
+        console.log(x--);
+    }
+}
+
+countDown(10);
