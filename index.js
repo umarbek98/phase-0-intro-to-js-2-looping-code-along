5154b070f7c6b6a6d97192d4a620b8c3f7ecf553
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-01-11 17:05:22 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+        const name = names[i]
+        const str = `Thank you, ${name}, for the wonderful ${event} gift!`
+        messages.push(str)
+    }
+    return messages
+}
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num--
+    }
+}
+console.log(countDown(-1))
